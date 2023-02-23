dbc8670648f6e24658f50fc71b6cf11eb4cc283e
Lissie Lessore <web3crafter@proton.me>
2023-02-23 15:25:46 +0100

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,55 @@
+const { network } = require("hardhat");
+const {
+    networkConfig,
+    developmentChains,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+// Ways to do the same:
+// ! hre = hardhat runtime enviroment
+// 1.
+// function deployFunc() {
+//    hre.getNamedAccounts()
+//    hre.deployments
+// }
+// module.exports.default = deployFunc;
+
+// 2.
+// module.exports = async (hre) => {
+//     const { getNamedAccounts, deployments } = hre;
+
+// };
+
+// 3.
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    let ethUsdPriceFeedAddress;
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
+    // when going for localhost or hardhat network we want to use a mock
+    const args = [ethUsdPriceFeedAddress];
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args);
+    }
+    log("-------------------------------------");
+};
+
+module.exports.tags = ["all", "fundme"];
---
+++ hardhat.config.js
@@ -0,0 +1,51 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
+module.exports = {
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+    },
+
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+    },
+
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+    },
+};
